d9e5d2cf7af4c67be522af43426556d9b7ab8b29
Javier Mendoza <jm9504202@gmail.com>
2019-10-24 14:18:14 -0700

    finished is_sorted -> working on bubble_sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -6,7 +6,11 @@ def is_sorted(items):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
-
+    for index, item in enumerate(items):
+        if index < len(items)-1:
+            if item > items[index + 1]:
+                return False
+    return True
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
@@ -15,6 +19,15 @@ def bubble_sort(items):
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Repeat until all items are in sorted order
     # TODO: Swap adjacent items that are out of order
+    for index, item in enumerate(items):
+        if index < len(items)-1:
+            if item > items[index + 1]:
+                items[index], items[index + 1]  = items[index + 1], items[index]
+                for index in range(index + 1):
+                    if items[index] > items[index + 1]:
+                        items[index], items[index + 1]  = items[index + 1], items[index]
+                
+    return items
 
 
 def selection_sort(items):
@@ -35,3 +48,12 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+
+if __name__ == '__main__':
+    array1 = [6, 7, 4, 5, 3, 1, 10, 2]
+    array1 = [6, 4, 5, 3, 1, 7, 2, 10]
+    array2 = [1, 2, 3, 4, 5, 6, 7, 8]
+    array3 = [1, 2, 3, 0, 5, 6, 7, 8]
+    print(bubble_sort(array1))
+    print(is_sorted(array2))
+    print(is_sorted(array3))
